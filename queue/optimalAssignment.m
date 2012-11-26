73c9216bb177abf742da4eca39d061f00886988d
James D. Brooks <brookj7@rpi.edu>
2012-11-26 15:12:37 -0500

    *** empty log message ***

--- queue/optimalAssignment.m
+++ queue/optimalAssignment.m
@@ -1,40 +1,97 @@
-function [u,w,q,x,lagrange] = optimalAssignment(mu,type,d,N)
+function [u,w,q,x,exit] = optimalAssignment(mu,type,d,N,METHOD)
 
     chains = find(type == 1);
     numChains = length(chains);
     cycles = find(type == 2);
     numCycles = length(cycles);
     
-    options = optimset('Algorithm', 'interior-point');
+    options = optimset('Algorithm', 'interior-point', 'TolX', 1e-12, ...
+                       'Display', 'iter');
 
     [x,fval, exit, output, lagrange] = ...
         fmincon(@(x)throughput(x), repmat(0,1,numChains*numCycles), [], ...
                 [], [], [],repmat(0,1,numChains*numCycles),[], ...
-                @(x)numEntities(x,mu,type,d,N), options);
+                @(x)numEntities(x,mu,type,d,N,METHOD), options);
     
-        
     centralFlow = zeros(1,numChains);
     cycleFlow = zeros(1,numCycles);
     
-    centralL = 0;
-    cycleL = 0;
-    travelL = 0;
+    centralL = zeros(1,numChains);
+    cycleL = zeros(1,numCycles);
+    travelL = sum(x.*d);
+    
+    centralW = zeros(1,numChains);
+    cycleW = zeros(1,numCycles);
+    
+    %    for i=1:numChains
+    %        centralFlow(i) = sum(x((i-1)*numCycles + 1:i*numCycles));
+    %        centralL = centralL + 1/(mu(chains(i))-centralFlow(i));
+    %    end
+    
+    %    for i = 1:numCycles
+    %        cycleFlow(i) = sum(x(i:numCycles:end));
+    %        cycleL = cycleL + 1/(mu(cycles(i))-cycleFlow(i));
+    %    end
     
     for i=1:numChains
-        centralFlow(i) = sum(x((i-1)*numCycles + 1:i*numCycles));
-        centralL = centralL + 1/(mu(chains(i))-centralFlow(i));
+        centralFlow(i) = sum(x((i-1)*numCycles + 1:i* ...
+                                    numCycles));
+        switch(METHOD)
+          case 1,                       % M/M/1
+            delta = centralFlow(i)/(mu(chains(i))-centralFlow(i));
+            pk = 0;
+            
+          case 2,                       % M/M/1/N
+            rho = centralFlow(i)/mu(chains(i));
+            if rho < 1
+                pk = ( rho^N*(1-rho) )/( 1 - rho^(N+1) );
+                Lq = rho/(1-rho) - rho*(N*rho^N + 1)/(1-rho^(N+1));
+ 
+            else
+                pk = 1/(N+1);
+                Lq = N*(N+1)/( 2*(N+1) );
+            end
+            delta = Lq + rho*(1-pk);
+        end
+        centralL(i) = delta; %min(max(0,delta), N);
+        centralW(i) = centralL(i)/( centralFlow(i)*(1-pk) );
     end
     
     for i = 1:numCycles
         cycleFlow(i) = sum(x(i:numCycles:end));
-        cycleL = cycleL + 1/(mu(cycles(i))-cycleFlow(i));
+
+        switch(METHOD)
+          case 1,                       % M/M/1
+            delta = cycleFlow(i)/(mu(cycles(i))-cycleFlow(i));
+            pk = 0;
+            
+          case 2,                       % M/M/1/N
+            rho = cycleFlow(i)/mu(cycles(i));
+            if rho < 1
+                pk = ( rho^N*(1-rho) )/( 1 - rho^(N+1) );
+                Lq = rho/(1-rho) - rho*(N*rho^N + 1)/(1-rho^(N+1));
+ 
+            else
+                pk = 1/(N+1);
+                Lq = N*(N+1)/( 2*(N+1) );
+            end
+            delta = Lq + rho*(1-pk);
+        end
+        
+        cycleL(i) = delta; %min(max(0,delta), N);
+        cycleW(i) = cycleL(i)/( cycleFlow(i)*(1-pk) );
     end
 
     % Outputs
     u = [centralFlow./mu(chains), cycleFlow./mu(cycles)];
-    w = [1./(mu(chains)-centralFlow), 1./(mu(cycles)-cycleFlow)];
-    q = [centralFlow./(mu(chains)-centralFlow), cycleFlow./ ...
-         (mu(cycles)-cycleFlow)];
+    switch(METHOD)
+      case 1,
+        w = [1./(mu(chains)-centralFlow), 1./(mu(cycles)- ...
+                                              cycleFlow)];
+      case 2,
+        w = [centralW, cycleW];
+    end
+    q = [centralL, cycleL, travelL];
     
     disp(sprintf('System Throughput: %2.2f', sum(centralFlow)));
 
@@ -45,7 +102,11 @@ function T = throughput(lambda)
     T = -sum(lambda);
 end
 
-function [C, Ceq] = numEntities(lambda, mu, type, d, N)
+function [C, Ceq] = numEntities(lambda, mu, type, d, N, METHOD)
+
+    epsilon = 1e-6;                     % required to ensure
+                                        % feasible solutions
+    
     chains = find(type == 1);
     numChains = length(chains);
     cycles = find(type == 2);
@@ -56,24 +117,58 @@ function [C, Ceq] = numEntities(lambda, mu, type, d, N)
     
     centralL = 0;
     cycleL = 0;
-    travelL = 0;
+    travelL = sum(lambda.*d);
     
     for i=1:numChains
         centralFlow(i) = sum(lambda((i-1)*numCycles + 1:i* ...
                                     numCycles));
-        delta = centralFlow(i)/(mu(chains(i))-centralFlow(i));
+        switch(METHOD)
+          case 1,                       % M/M/1
+            delta = centralFlow(i)/(mu(chains(i))-centralFlow(i));
+            pk = 0;
+            
+          case 2,                       % M/M/1/N
+            rho = centralFlow(i)/mu(chains(i));
+            if rho < 1
+                pk = ( rho^N*(1-rho) )/( 1 - rho^(N+1) );
+                Lq = rho/(1-rho) - rho*(N*rho^N + 1)/(1-rho^(N+1));
+ 
+            else
+                pk = 1/(N+1);
+                Lq = N*(N+1)/( 2*(N+1) );
+            end
+            delta = Lq + rho*(1-pk);
+        end
         centralL = centralL + delta; %min(max(0,delta), N);
     end
     
     for i = 1:numCycles
         cycleFlow(i) = sum(lambda(i:numCycles:end));
-        delta = cycleFlow(i)/(mu(cycles(i))-cycleFlow(i));
+
+        switch(METHOD)
+          case 1,                       % M/M/1
+            delta = cycleFlow(i)/(mu(cycles(i))-cycleFlow(i));
+            pk = 0;
+            
+          case 2,                       % M/M/1/N
+            rho = cycleFlow(i)/mu(cycles(i));
+            if rho < 1
+                pk = ( rho^N*(1-rho) )/( 1 - rho^(N+1) );
+                Lq = rho/(1-rho) - rho*(N*rho^N + 1)/(1-rho^(N+1));
+ 
+            else
+                pk = 1/(N+1);
+                Lq = N*(N+1)/( 2*(N+1) );
+            end
+            delta = Lq + rho*(1-pk);
+        end
+        
         cycleL = cycleL + delta; %min(max(0,delta), N);
     end
     
-    C = [ centralL + cycleL + travelL - N , ... % total number
-          centralFlow - mu(chains), ...         % stability of chains
-          cycleFlow - mu(cycles) ];             % stability of cycles
+    C = [ centralL + cycleL + travelL - N , ...% total number
+          centralFlow-mu(chains)+epsilon, ...  % stability of chains
+          cycleFlow-mu(cycles)+epsilon];       % stability of cycles
     
     Ceq = [];
 end
--- queue/optimalTest.m
+++ queue/optimalTest.m
@@ -7,11 +7,33 @@
 close all;
 clear all;
 
-type = [1 1 1 2 2 2 2 2 2 2 2 2 2 2];
+OPTION = 1;
+% 1 - new random data w/o delay
+% 2 - new random data w/ delay
+% 3 - new random N, same mus w/o delay (load file below)
+% 4 - same N, mus, w/ random delay (load file below)
+
+if OPTION  > 2                          % load data
+    load rand_2_6_20nov2012;
+    
+    if OPTION == 3
+        entities = zeros(1,NUM_TESTS)*NaN;
+    end
+
+else                                    % else new data/structure
+    type = [1 1 2 2 2 2 2 2];
+    NUM_TESTS = 100;
+    serviceRates = zeros(1,NUM_TESTS*length(type));
+    entities = zeros(1,NUM_TESTS)*NaN;
+end
+
 cycles = find(type == 2);
 chains = find(type == 1);
-d = 0;
-NUM_TESTS = 100;
+
+numCycles = length(cycles);
+numChains = length(chains);
+numStations = length(type);
+decisionVars = numCycles*numChains;
 
 % Output measures
 cycleEquity = zeros(1,NUM_TESTS)*NaN;
@@ -20,52 +42,144 @@ flowEquity = zeros(1,NUM_TESTS)*NaN;
 centralDiff = zeros(1,NUM_TESTS)*NaN;
 cycleDiff = zeros(1,NUM_TESTS)*NaN;
 partition = zeros(1,NUM_TESTS)*NaN;
-entities = zeros(1,NUM_TESTS)*NaN;
 throughput = zeros(1,NUM_TESTS)*NaN;
 centralCapacity = zeros(1,NUM_TESTS)*NaN;
 cycleCapacity = zeros(1,NUM_TESTS)*NaN;
 
-figure(1); subplot(311), title('wait times, 1-2'), hold on;
-subplot(312), title('wait times, 2-3'), hold on;
-subplot(313), title('wait times, 1-3'), hold on;
+% Raw data
+flows = zeros(1,NUM_TESTS*decisionVars);
+travelDelay = zeros(1,NUM_TESTS*decisionVars);
+waitTimes = zeros(1,NUM_TESTS*numStations);
+utilizations = zeros(1,NUM_TESTS*numStations);
+
+figure(1); subplot(311), title('wait times, 1p-2p'), hold on;
+subplot(312), title('wait times, 2p-3p'), hold on;
+subplot(313), title('wait times, 1p-3p'), hold on;
+
+figure(2); subplot(311), title('wait times 1c-2c'), hold on;
+subplot(312), title('wait times, 2c-3p'), hold on;
+subplot(313), title('wait times, 1c-5p'), hold on;
 
 for i = 1:NUM_TESTS
-    N = randi(length(type)*5);          % number of entities
-    entities(i) = N;
-    mu = randi(20, 1, length(type))+5;  % 5-25 service rates
+    if OPTION < 4
+        N = randi(length(type)*2);      % number of entities
+        entities(i) = N;
+    else
+        N = entities(i);                % get from file
+    end
+    
+    if OPTION < 3
+        mu = [randi(40, 1, length(chains))+5, randi(20,1,length(cycles)) ...
+              + 10];                    % random service rates
+    else                                % get from file
+        mu = serviceRates((i-1)*numStations + 1 : i*numStations);
+    end
+
+    d = zeros(1,decisionVars); 
+    
+    if OPTION == 2 || OPTION == 4
+        d = rand(1, decisionVars)*0.9+0.1; % U(0.1,1)
+    end
+
+    % Summary of random rates
     centralDiff(i) = max(mu(chains)) - min(mu(chains));
     cycleDiff(i) = max(mu(cycles)) - min(mu(cycles));
-    [u,w,q,x,lagrange] = optimalAssignment(mu,type,d,N);
+
+    % Solve
+    [u,w,q,x,flag] = optimalAssignment(mu,type,d,N);
+    
+    % Test with proportional routing
+    %    [u2,w2,q2,x2] = queueSim(N,mu,);
+
+    % Calculate summary metrics
     cycleEquity(i) = max(u(cycles)) - min(u(cycles));
     flowEquity(i) = max(x) - min(x);
     centralEquity(i) = max(u(chains)) - min(u(chains));
-    partition(i) = length(find(x < 0.1));
+    partition(i) = length(find(x < 0.01));
     throughput(i) = sum(x);
     centralCapacity(i) = throughput(i)/sum(mu(chains));
     cycleCapacity(i) = throughput(i)/sum(mu(cycles));
-    figure(1);
-    if centralCapacity(i) < 0.9
-        subplot(311), plot(sqrt(mu(cycles(1)))*w(cycles(1)), sqrt(mu(cycles(2)))*w(cycles(2)), 'b.');
-        subplot(312), plot(sqrt(mu(cycles(2)))*w(cycles(2)), sqrt(mu(cycles(3)))*w(cycles(3)), 'b.');
-        subplot(313), plot(sqrt(mu(cycles(1)))*w(cycles(1)), sqrt(mu(cycles(3)))*w(cycles(3)), 'b.');
+    
+    % stash raw data
+    flows((i-1)*decisionVars + 1 : i*decisionVars) = x;
+    travelDelay((i-1)*decisionVars + 1 : i*decisionVars) = d;
+    waitTimes((i-1)*numStations + 1 : i*numStations) = w;
+    utilizations((i-1)*numStations + 1 : i*numStations) = u;
+    serviceRates((i-1)*numStations + 1 : i*numStations) = mu;
+    
+
+    if ( flag == 1 ) & ( sum(x>0.01) == length(x) )%centralCapacity(i) < 0.9
+        figure(1);        
+        subplot(311), plot(sqrt(mu(cycles(1)))*w(cycles(1)), ...
+                           sqrt(mu(cycles(2)))*w(cycles(2)), 'b.');
+        subplot(312), plot(sqrt(mu(cycles(2)))*w(cycles(2)), ...
+                           sqrt(mu(cycles(3)))*w(cycles(3)), 'b.');
+        subplot(313), plot(sqrt(mu(cycles(1)))*w(cycles(1)), ...
+                           sqrt(mu(cycles(3)))*w(cycles(3)), 'b.');
+        figure(2);
+        subplot(311), plot(sqrt(mu(chains(1)))*w(chains(1)), ...
+                           sqrt(mu(chains(2)))*w(chains(2)), 'b.');
+        subplot(312), plot(sqrt(mu(chains(2)))*w(chains(2)), ...
+                           sqrt(mu(cycles(3)))*w(cycles(3)), 'b.');
+        subplot(313), plot(sqrt(mu(chains(1)))*w(chains(1)), ...
+                           sqrt(mu(cycles(5)))*w(cycles(5)), 'b.');
+                       
+%         figure(3);        
+%         subplot(311), plot(sqrt(mu(cycles(1)))*w(cycles(1)), ...
+%                            sqrt(mu(cycles(2)))*w(cycles(2)), 'b.');
+%         subplot(312), plot(sqrt(mu(cycles(2)))*w(cycles(2)), ...
+%                            sqrt(mu(cycles(3)))*w(cycles(3)), 'b.');
+%         subplot(313), plot(sqrt(mu(cycles(1)))*w(cycles(1)), ...
+%                            sqrt(mu(cycles(3)))*w(cycles(3)), 'b.');
     else
-        subplot(311), plot(sqrt(mu(cycles(1)))*w(cycles(1)), sqrt(mu(cycles(2)))*w(cycles(2)), 'r.');
-        subplot(312), plot(sqrt(mu(cycles(2)))*w(cycles(2)), sqrt(mu(cycles(3)))*w(cycles(3)), 'r.');
-        subplot(313), plot(sqrt(mu(cycles(1)))*w(cycles(1)), sqrt(mu(cycles(3)))*w(cycles(3)), 'r.');
+        figure(1);
+        subplot(311), plot(sqrt(mu(cycles(1)))*w(cycles(1)), ...
+                           sqrt(mu(cycles(2)))*w(cycles(2)), 'r.');
+        subplot(312), plot(sqrt(mu(cycles(2)))*w(cycles(2)), ...
+                           sqrt(mu(cycles(3)))*w(cycles(3)), 'r.');
+        subplot(313), plot(sqrt(mu(cycles(1)))*w(cycles(1)), ...
+                           sqrt(mu(cycles(3)))*w(cycles(3)), 'r.');
+        figure(2);
+        subplot(311), plot(sqrt(mu(chains(1)))*w(chains(1)), ...
+                           sqrt(mu(chains(2)))*w(chains(2)), 'r.');
+        subplot(312), plot(sqrt(mu(chains(2)))*w(chains(2)), ...
+                           sqrt(mu(cycles(3)))*w(cycles(3)), 'r.');
+        subplot(313), plot(sqrt(mu(chains(1)))*w(chains(1)), ...
+                           sqrt(mu(cycles(5)))*w(cycles(5)), 'r.');
     end
 end
 
-figure, plot(cycleDiff, 'b.'), title(['Max. Cycle Service Rate ' ...
-                    'Difference']);
-figure, plot(centralDiff, 'b.'), title(['Max. Central Service Rate ' ...
-                    'Difference']);
-figure, plot(cycleEquity, 'b.'), title(['Max. Cycle Utilization Rate ' ...
-                    'Difference']);
-figure, plot(centralEquity, 'b.'), title(['Max. Central Utilization Rate ' ...
-                    'Difference']);
-figure, plot(partition./length(x), 'b.'), title(['Percentage of flows ' ...
-                    'nominally zero']);
+figure, plot(cycleDiff, 'b.');
+title(['Max. Cycle Service Rate ' ...
+       'Difference']);
+
+figure, plot(centralDiff, 'b.');
+title(['Max. Central Service Rate ' ...
+       'Difference']);
+
+figure, plot(cycleEquity, 'b.');
+title(['Max. Cycle Utilization Rate ' ...
+       'Difference']);
+
+figure, plot(centralEquity, 'b.');
+title(['Max. Central Utilization Rate ' ...
+       'Difference']);
+
+figure, plot(flowEquity, 'b.');
+title(['Max. Flow Rate ' ...
+       'Difference']);
+
+figure, plot(partition./length(x), 'b.');
+title(['Percentage of flows ' ...
+       'nominally zero']);
 
 figure, plot(centralCapacity, 'b.'), hold on, 
 plot(cycleCapacity, 'r.');
 title('Capacities');
+
+%const =   zeros(1,decisionVars); 
+%for i = 1:length(chains)
+%    for j = 1:length(cycles)
+%        const((i-1)*length(cycles) + j) = mu(chains(i))*w(chains(i))^2 + mu(cycles(j))*w(cycles(j))^2 + d((i-1)*length(chains) + j)
+%    end
+%end
